7f1e7a98a41d5ccea04355ffc09bfb6edd87faf0
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-03-11 15:17:22 +0700

    Add test for summoner controller

--- src/modules/summoners/summoners.controller.spec.ts
+++ src/modules/summoners/summoners.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SummonersController } from './summoners.controller';
+import {rootMongooseTestModule} from "../../shares/utils";
+import {MongooseModule} from "@nestjs/mongoose";
+import {Summoners, SummonersSchema} from "./summoners.schema";
+import {SummonersService} from "./summoners.service";
+import {SummonerNotExistException} from "./summoners.exceptions";
 
 describe('SummonersController', () => {
   let controller: SummonersController;
+  let service: SummonersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([{ name: Summoners.name, schema: SummonersSchema }]),
+      ],
       controllers: [SummonersController],
+      providers: [SummonersService],
     }).compile();
 
     controller = module.get<SummonersController>(SummonersController);
+    service = module.get<SummonersService>(SummonersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
+
+  it('summoner get info correct', async () => {
+    await service.createRandomSummoner('0x123', 1);
+    const summoner = await controller.getPublicSummonerInfo(1);
+    expect(summoner.summonerId).toEqual(1);
+  });
+
+  it(`Throw exception when get incorrect summonerId`, async () => {
+    await expect(controller.getPublicSummonerInfo(1))
+      .rejects.toThrowError(SummonerNotExistException);
+  })
 });
--- src/modules/summoners/summoners.service.spec.ts
+++ src/modules/summoners/summoners.service.spec.ts
@@ -1,9 +1,9 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SummonersService } from './summoners.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {SummonersService} from './summoners.service';
 import {Summoners, SummonersSchema} from "./summoners.schema";
 import {closeInMongodConnection, rootMongooseTestModule} from "../../shares/utils";
 import {MongooseModule} from "@nestjs/mongoose";
-import exp from "constants";
+import {SummonersClass} from "./summoners.cls.enum";
 
 describe('SummonersService', () => {
   let service: SummonersService;
@@ -30,6 +30,9 @@ describe('SummonersService', () => {
     expect(summoner.summonerId).toEqual(1);
     expect(summoner.strength).toBeGreaterThanOrEqual(1);
     expect(summoner.strength).toBeLessThan(100);
+    expect(summoner.power).toBeGreaterThanOrEqual(1);
+    expect(summoner.power).toBeLessThan(100);
+    expect([SummonersClass.FARMER, SummonersClass.FIGHTER, SummonersClass.HEALER]).toContain(summoner.cls);
   });
 
   afterAll(async () => {
