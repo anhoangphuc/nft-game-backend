d3815298b6d30d2bd993bc4d177a43b9b0222922
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-03-11 14:37:47 +0700

    Add test for method create a random summoner

--- src/modules/summoners/summoners.service.spec.ts
+++ src/modules/summoners/summoners.service.spec.ts
@@ -1,11 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SummonersService } from './summoners.service';
+import {Summoners, SummonersSchema} from "./summoners.schema";
+import {closeInMongodConnection, rootMongooseTestModule} from "../../shares/utils";
+import {MongooseModule} from "@nestjs/mongoose";
+import exp from "constants";
 
 describe('SummonersService', () => {
   let service: SummonersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([{ name: Summoners.name, schema: SummonersSchema }]),
+      ],
       providers: [SummonersService],
     }).compile();
 
@@ -14,5 +22,17 @@ describe('SummonersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it(`Create a random summoner success`, async () => {
+    const summoner = await service.createRandomSummoner('0x123', 1);
+    expect(summoner).toBeDefined();
+    expect(summoner.summonerId).toEqual(1);
+    expect(summoner.strength).toBeGreaterThanOrEqual(1);
+    expect(summoner.strength).toBeLessThan(100);
+  });
+
+  afterAll(async () => {
+    await closeInMongodConnection();
   });
 });
--- src/shares/utils.ts
+++ src/shares/utils.ts
@@ -1,5 +1,23 @@
+import { MongoMemoryServer} from "mongodb-memory-server";
+import {MongooseModule, MongooseModuleOptions} from "@nestjs/mongoose";
 export function randomMinMax(minNumber: number, maxNumber: number) {
   //Include minValue, exclude maxValue
   const diff = maxNumber - minNumber;
   return Math.floor(Math.random() * diff) + minNumber;
+}
+
+let mongod: MongoMemoryServer;
+export const rootMongooseTestModule = (options: MongooseModuleOptions = {}) => MongooseModule.forRootAsync({
+  useFactory: async () => {
+    mongod = await MongoMemoryServer.create();
+    const mongoUri = mongod.getUri();
+    return {
+      uri: mongoUri,
+      ...options,
+    }
+  }
+});
+
+export const closeInMongodConnection = async () => {
+  if (mongod) await mongod.stop();
 }
