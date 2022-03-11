f2ce51352859a59e6ffbe8bd52eb716551c7eebf
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-03-11 21:01:44 +0700

    Add base-event schema

---
+++ src/shares/base-event.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+
+@Schema()
+export class BaseEvent {
+  @Prop({
+    type: String,
+  })
+  tx: string;
+
+  @Prop({
+    type: Number,
+  })
+  blockNumber: number;
+
+  @Prop({
+    type: Number,
+  })
+  logIndex: number;
+
+  @Prop({
+    type: Number,
+  })
+  blockTimestamp: number;
+}
