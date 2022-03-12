537a5e90eec54bab611b9c08520feb44f8233c78
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-03-12 15:24:36 +0700

    Add method callWithChunk

--- src/shares/utils.ts
+++ src/shares/utils.ts
@@ -30,3 +30,17 @@ export const closeInMongodConnection = async () => {
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export const callWithChunk = async (events: any[], callback: (array: any[]) => Promise<any> | any, chunk_size = 50) => {
+  if (!Array.isArray(events)) {
+    console.log('must be an array');
+    return;
+  }
+  const eventsLength = events.length;
+  for (let index = 0; index < eventsLength; index += chunk_size) {
+    const eventsChunk = events.slice(index, index + chunk_size);
+    console.log(`processing ${callback.name} ${index + eventsChunk.length}/${eventsLength}`);
+
+    await callback(eventsChunk);
+  }
+};
